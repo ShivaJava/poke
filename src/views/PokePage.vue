f0a84410e0eab92e57810385824ee7e03ba0dea4
Oleg Shiva <shivacode.mail@gmail.com>
2020-03-22 23:57:20 +0300

    Вывел спрайты для Покемона на отдельной странице

--- src/views/PokePage.vue
+++ src/views/PokePage.vue
@@ -1,10 +1,25 @@
 <template>
-    <b-container>
-        <h1>{{pokeData.name}}</h1>
-    <pre>
-        {{pokeData}}
-    </pre>
-    </b-container>
+    <div class="PokePage">
+        <b-container
+            v-if="pokeData && loaded"
+            class="pt-5"
+        >
+            <b-row>
+                <b-col>
+                    <h1 class="h3 text-uppercase mb-4">Pokemon <span class="text-danger">{{pokeData.name}}</span></h1>
+                </b-col>
+            </b-row>
+            <b-row>
+                <b-col>
+                    <b-img 
+                        v-for="(item, index) in pokeSprites" 
+                        class="border border-dark rounded mr-2 mb-2 p-2"
+                        v-bind:src="item"
+                        v-bind:key="index" />
+                </b-col>
+            </b-row>
+        </b-container>
+    </div>
 </template>
 
 <script>
@@ -15,16 +30,26 @@ export default {
     data: () => {
       return {
         pokeData: null,
+        pokeSprites: null,
         loaded: false,
       }
     },
     created() {
       axios.get(`${process.env.VUE_APP_API_HOST}/pokemon/${this.$route.params.name}`)
         .then(({data}) => {          
-          this.pokeData = data
-          setTimeout(() => {
-            this.loaded = true
-          }, 500)
+            this.pokeData = data
+
+            let cleanPokeSprites = {}
+            for(let item in this.pokeData.sprites) {
+                let itemValue = this.pokeData.sprites[item]
+                if(!itemValue) continue
+                cleanPokeSprites[item] = itemValue
+            }    
+            this.pokeSprites = cleanPokeSprites
+            
+            setTimeout(() => {
+                this.loaded = true
+            }, 500) 
         })
         .catch(error => console.log(error))
         
